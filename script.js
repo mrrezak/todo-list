9c4a5ab7812570fcb78237f8dc2ae8b36597f0dd
Кирилл Зуев <kirillzuev@iMac-Kirill.local>
2021-08-26 23:39:19 +0300

    first

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-description.addEventListener("click", btnClick);
+btn.addEventListener("click", btnClick);
 
 function btnClick() {
     var name = document.getElementById("name").value;
     var description = document.getElementById("description").value;
     console.log(name);
     console.log(description);
+    let div = document.createElement('div');
+    div.className = "form__rectangle";
+    div.innerHTML = "<h1 class=form__title>test</h1>"; 
 
+    let list = document.getElementById("insert");
+    list.append(div);
+    name.reset()
+
+ 
 }
