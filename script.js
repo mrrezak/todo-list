ed1037618a5b93892a63540dd21d7dc94a46d965
Кирилл Зуев <kirillzuev@iMac-Kirill.local>
2021-08-26 22:02:56 +0300

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+description.addEventListener("click", btnClick);
+
+function btnClick() {
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+    console.log(name);
+    console.log(description);
+
+}
